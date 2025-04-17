61a972d1cc4d1c28b3142ea23af50c3fd06bfe8b
Royal-design <royaldesign1999@gmail.com>
2025-04-17 15:29:35 +0100

    updated

--- src/Layout/PublicLayout.tsx
+++ src/Layout/PublicLayout.tsx
@@ -3,14 +3,6 @@ import { UseAuthContext } from "../Context/AuthContext/UseAuthContext";
 
 const PublicLayout = () => {
   const { user } = UseAuthContext();
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (!user) {
-  //       setUserLoading(false);
-  //     }
-  //   }, 1000);
-  // }, []);
-  // if (userLoading) return <div>Loading...</div>;
 
   if (user) return <Navigate to="/" />;
 
